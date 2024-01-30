48bbe3235321cb6aef7946dd3b8daddd2789c883
Adam7742 <156726734+Adam7742@users.noreply.github.com>
2024-01-29 20:27:45 -0800

    Add files via upload

---
+++ blockface.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(130, 200, 130);
+  fill(45, 23, 150)
+  circle (100, 100, 100)
+  fill (45, 23, 150);
+  strokeWeight(5);
+  stroke(80);
+  ellipse(300, 100, 100, 100);
+  fill(250, 60, 60)
+  square(100, 200, 200, 20)
+}
